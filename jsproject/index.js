c046507afd19cc3ca1e312342fd2a30de5169438
Htin Kyaw <htinkyaw@Htins-MacBook-Air.local>
2026-03-29 10:53:17 +0900

    third-commit

--- jsproject/index.js
+++ jsproject/index.js
@@ -1,12 +1,15 @@
 let errMessage1 = document.getElementById("msg-1");
 let errMessage2 = document.getElementById("msg-2");
 let errMessage3 = document.getElementById("msg-3");
+let btn_1 = document.getElementById("btn-1");
+let btn_2 = document.getElementById("btn-2");
+
 
 // console.log(errMessage1,errMessage2)
 let guessedNum = [];
 let chance = 10;
 let count = 0;
-const randomNumber = Math.floor(Math.random() * 50) + 1;
+let randomNumber = Math.floor(Math.random() * 30) + 1;
 console.log(randomNumber);
 
 let guess = () => {
@@ -17,6 +20,7 @@ let guess = () => {
   if (userGuess === randomNumber) {
     errMessage1.textContent = `Congratulations! You Won in ${count} time!`;
     errMessage1.style.color = '#fff';
+    btn_1.disabled = "true";
     
   } else {
     if (userGuess < 0 || userGuess > 50) {
@@ -27,8 +31,22 @@ let guess = () => {
       errMessage1.textContent = `သင့်numberက'နည်း'နေပါတယ်။ထပ်ကြိုးစားကြည့်ပါဦး^^`;
     }
   }
+  if (chance <= 0) {
+      errMessage1.textContent = `Game Over! ၁၀ကြိမ်ပြည့်သွားပါပြီ။ Restart ပြန်လုပ်ပါ။`;
+      btn_1.disabled = "true";
+    }
   guessedNum.push(userGuess);
   errMessage2.textContent = "ထပ်ကြိုးစားခွင့် : " + chance + " ကြိမ်";
   errMessage3.textContent = "ခန့်မှန်းပြီးတဲ့ number : "+guessedNum;
   document.querySelector(".guess").value = "";
 };
+let restart = () => {
+  randomNumber = Math.floor(Math.random() * 30) + 1;
+  console.log(randomNumber);
+  chance = 10;
+  count = 0;
+  guessedNum =[];
+  errMessage2.textContent = "ထပ်ကြိုးစားခွင့် : ";
+  errMessage3.textContent = "ခန့်မှန်းပြီးတဲ့ number : ";
+  errMessage1.textContent = "";
+}
